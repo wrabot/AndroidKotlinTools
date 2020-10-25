9ae1860dd24a05f64c9701ab821a8c370a59aa77
Wilfrid Rabot <wilfird.rabot@gmail.com>
2020-10-25 21:36:19 +0100

    Remove tools base

--- tools-base/src/main/java/com/wrabot/tools/Enum.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("unused")
-
-package com.wrabot.tools
-
-/**
- * Gets enum value for [name] or a [default] value if [name] is not found.
- */
-inline fun <reified T : Enum<*>> enumValueOrDefault(name: String, default: T? = null): T? =
-        T::class.java.enumConstants?.firstOrNull { it.name == name } ?: default
