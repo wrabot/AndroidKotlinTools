7b37b610ada723e53c93a4134790db024952c19c
Wilfrid Rabot <wilfrid.rabot@orange.com>
2022-11-14 11:52:33 +0100

    Improve preview

--- tools-compose/src/main/java/com/wrabot/tools/compose/CrossSlide.kt
+++ tools-compose/src/main/java/com/wrabot/tools/compose/CrossSlide.kt
@@ -18,29 +18,39 @@ package com.wrabot.tools.compose
 import androidx.compose.animation.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import kotlin.math.sign
 
-@OptIn(ExperimentalTextApi::class)
 @Preview
 @Composable
 fun CrossSlidePreview() {
     var state by remember { mutableStateOf(0) }
-    val textMeasurer = rememberTextMeasurer()
     CrossSlide(state, Modifier.size(100.dp).clickable { state = (state + 1) % 4 }) {
-        Canvas(Modifier.fillMaxSize()) {
-            drawText(textMeasurer, it.toString(), Offset(size.width / 2, size.height / 2))
+        Box(contentAlignment = Alignment.Center) {
+            Text(AnnotatedString(it.toString()))
         }
     }
+}
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+private fun Text(text: AnnotatedString) {
+    val textMeasurer = rememberTextMeasurer()
+    val textLayoutResult = textMeasurer.measure(text)
+    val size = with(LocalDensity.current) { textLayoutResult.size.toSize().toDpSize() }
+    Canvas(Modifier.size(size)) { drawText(textLayoutResult) }
 }
 
 /**
