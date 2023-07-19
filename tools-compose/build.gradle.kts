e98ceae66d855623284b51ad9192bc74a8873e14
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-07-19 09:19:02 +0200

    Update project

--- tools-compose/build.gradle.kts
+++ tools-compose/build.gradle.kts
@@ -1,10 +1,10 @@
 android {
     buildFeatures.compose = true
-    composeOptions.kotlinCompilerExtensionVersion = "1.3.2"
+    composeOptions.kotlinCompilerExtensionVersion = "1.4.8"
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2022.11.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
