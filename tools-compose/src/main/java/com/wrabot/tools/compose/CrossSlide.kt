8ce6d055215c8abf419bdbd26f0bb06c19e506b3
Wilfrid <wilfrid.rabot@gmail.com>
2023-11-11 18:45:15 +0100

    More generic CrossSlide

--- tools-compose/src/main/java/com/wrabot/tools/compose/CrossSlide.kt
+++ tools-compose/src/main/java/com/wrabot/tools/compose/CrossSlide.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,24 +35,17 @@ import kotlin.math.sign
 @Preview
 @Composable
 fun CrossSlidePreview() {
-    var state by remember { mutableStateOf(0) }
-    CrossSlide(state,
+    var state by remember { mutableIntStateOf(0) }
+    CrossSlide(
+        state,
         Modifier
             .size(100.dp)
-            .clickable { state = (state + 1) % 4 }, "slide") {
+            .clickable { state = (state + 1) % 4 }, "slide"
+    ) {
         Box(contentAlignment = Alignment.Center) {
-            Text(AnnotatedString(it.toString()))
+            Text(text = AnnotatedString(it.toString()))
         }
     }
-}
-
-@OptIn(ExperimentalTextApi::class)
-@Composable
-private fun Text(text: AnnotatedString) {
-    val textMeasurer = rememberTextMeasurer()
-    val textLayoutResult = textMeasurer.measure(text)
-    val size = with(LocalDensity.current) { textLayoutResult.size.toSize().toDpSize() }
-    Canvas(Modifier.size(size)) { drawText(textLayoutResult) }
 }
 
 /**
@@ -76,10 +68,81 @@ private fun Text(text: AnnotatedString) {
  * This is critical to ensure a successful lookup of all the incoming and outgoing content during
  * content transform.
  */
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun <T : Comparable<T>> CrossSlide(
     targetState: T,
+    modifier: Modifier = Modifier,
+    label: String = "CrossSlide",
+    content: @Composable (T) -> Unit
+) = CrossSlide(
+    targetState = targetState,
+    compareTo = { this.compareTo(it) },
+    modifier = modifier,
+    label = label,
+    content = content
+)
+
+/**
+ * [CrossSlide] is a container that slides its content when [targetState] changes.
+ * Its [content] for different target states is defined in a mapping between a target state
+ * and a composable function.
+ *
+ * When [targetState] changes, content for both new and previous targetState will be looked up
+ * through the [content] lambda.
+ * The [targetState] must be a comparable in order to decide which animation will be execute:
+ * If [targetState] is not null and current state is null, the current state's content slide out to left
+ * and the target state's content slide in from right
+ * If [targetState] is null and current state is not null, the current state's content slide in from left
+ * and the target state's content slide out to right
+ * If [targetState] and current state are both not null, the current state's content fade out and
+ * the target state's content fade in
+ *
+ * **IMPORTANT**: The targetState parameter for the [content] lambda should *always* be taken
+ * into account in deciding what composable function to return as the content for that state.
+ * This is critical to ensure a successful lookup of all the incoming and outgoing content during
+ * content transform.
+ */
+@Composable
+fun <T> CrossSlide(
+    targetState: T,
+    modifier: Modifier = Modifier,
+    label: String = "CrossSlide",
+    content: @Composable (T) -> Unit
+) = CrossSlide(
+    targetState = targetState,
+    compareTo = { ifNull0Else1() - it.ifNull0Else1() },
+    modifier = modifier,
+    label = label,
+    content = content
+)
+
+private fun <T> T.ifNull0Else1() = if (this == null) 0 else 1
+
+/**
+ * [CrossSlide] is a container that slides its content when [targetState] changes.
+ * Its [content] for different target states is defined in a mapping between a target state
+ * and a composable function.
+ *
+ * When [targetState] changes, content for both new and previous targetState will be looked up
+ * through the [content] lambda.
+ *
+ * The [compareTo] is used to compare states in order to decide which animation will be execute:
+ * If [targetState] is greater than current state, the current state's content slide out to left
+ * and the target state's content slide in from right
+ * If [targetState] is lesser than current state, the current state's content slide in from left
+ * and the target state's content slide out to right
+ * If [targetState] equals current state, the current state's content fade out and
+ * the target state's content fade in
+ *
+ * **IMPORTANT**: The targetState parameter for the [content] lambda should *always* be taken
+ * into account in deciding what composable function to return as the content for that state.
+ * This is critical to ensure a successful lookup of all the incoming and outgoing content during
+ * content transform.
+ */
+@Composable
+fun <T> CrossSlide(
+    targetState: T,
+    compareTo: T.(T) -> Int,
     modifier: Modifier = Modifier,
     label: String = "CrossSlide",
     content: @Composable (T) -> Unit
@@ -96,4 +159,13 @@ fun <T : Comparable<T>> CrossSlide(
     label = label
 ) {
     content(it)
+}
+
+// for preview
+@Composable
+private fun Text(text: AnnotatedString) {
+    val textMeasurer = rememberTextMeasurer()
+    val textLayoutResult = textMeasurer.measure(text)
+    val size = with(LocalDensity.current) { textLayoutResult.size.toSize().toDpSize() }
+    Canvas(Modifier.size(size)) { drawText(textLayoutResult) }
 }
