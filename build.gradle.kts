0edf8b502f0b15eb4f71deba08231f580c8cbc20
Wilfrid <wilfrid.rabot@gmail.com>
2023-11-11 18:46:19 +0100

    Cleaning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.gradle.internal.scope.ProjectInfo.Companion.getBaseName
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
